e3eae0e3170fde8f8cb6cde1060d0bec502a9c81
Isaías Santana <isds.santana@gmail.com>
2024-04-24 23:53:58 -0300

    initial commit

---
+++ ISAWebSocketExample/ContentView.swift
@@ -0,0 +1,57 @@
+//
+//  ContentView.swift
+//  ISAWebSocketExample
+//
+//  Created by Isaías Santana on 24/04/24.
+//
+
+import SwiftUI
+import ISAWebSocket
+
+final class SocketHandler: ISAWebSocketDelegate {
+    let socket = ISAWebSocket(url: URL(string: "wss://echo.websocket.org/")!)
+
+    init() {
+        socket.delegate = self
+    }
+
+    func socket(_ socket: WebSocketClient, didReceiveMessage message: SocketMessage) {
+        switch message {
+        case .string(let string):
+            print("Message \(string)")
+        case .data(let data):
+            print("data receivd \(data)")
+        }
+    }
+
+    func socket(_ socket: WebSocketClient, didReceiveError error: SocketError) {
+        print("error \(error)")
+    }
+
+    func socketDidCloseConnection(_ socket: WebSocketClient) {
+        print("closed connection")
+    }
+}
+
+struct ContentView: View {
+    @State private var handler = SocketHandler()
+
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Button("Send Message", action: {
+                handler.socket.send(message: .string("Hello message"))
+            })
+        }
+        .padding()
+        .onAppear(perform: {
+            handler.socket.startConnection()
+        })
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ISAWebSocketExample/ISAWebSocketExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ISAWebSocketExampleApp.swift
+//  ISAWebSocketExample
+//
+//  Created by Isaías Santana on 24/04/24.
+//
+
+import SwiftUI
+
+@main
+struct ISAWebSocketExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
